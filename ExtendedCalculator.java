cd7e385a61748a2ef49f7c5fc85211d4d98d7ba5
Gousiya Begum <01fe22bca072@kletech.ac.in>
2024-03-26 00:44:05 +0530

    Code for Square and Cube Functionalities

---
+++ ExtendedCalculator.java
@@ -0,0 +1,9 @@
+class ExtendedCalculator extends Calculator {
+   public float square(float num) {
+       return (num * num);
+   }
+
+   public float cube(float num) {
+       return (num * num * num);
+   }
+}
